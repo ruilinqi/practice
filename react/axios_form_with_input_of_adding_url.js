4c2c4f4d66884443ba9bb600a4a159e37e4a39e4
ruilinqi <ruilinqi7@gmail.com>
2023-04-06 16:42:23 -0400

    Update answer of react form of displaying url content

--- react/axios_form_with_input_of_adding_url.js
+++ react/axios_form_with_input_of_adding_url.js
@@ -5,3 +5,42 @@ https://pokeapi.co/api/v2/pokemon
 https://rickandmortyapi.com/api/character
 
  */
+
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [url, setUrl] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.get(url);
+      setResponse(JSON.stringify(res.data, null, 2));
+    } catch (error) {
+      console.error(error);
+      setResponse(`Error: ${error.message}`);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter a URL:
+          <input
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <pre>{response}</pre>
+    </div>
+  );
+}
+
+export default App;
