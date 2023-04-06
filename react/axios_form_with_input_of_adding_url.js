6ca71361a87a4f855c98656da7098cb343c48e37
ruilinqi <ruilinqi7@gmail.com>
2023-04-06 16:42:06 -0400

    Add question of react form of displaying url content

---
+++ react/axios_form_with_input_of_adding_url.js
@@ -0,0 +1,7 @@
+/**
+ * Create react app, create component including a form with one input to add any url and 
+ * submit to display results on page.
+https://pokeapi.co/api/v2/pokemon
+https://rickandmortyapi.com/api/character
+
+ */
